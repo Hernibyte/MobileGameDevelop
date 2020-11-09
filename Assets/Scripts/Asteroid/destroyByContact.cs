b14e2821e8873f15503b98816f0fcf987cb860e7
Gossa Hernan <hernanagustin.g24@hotmail.com>
2020-11-08 22:07:02 -0300

    GameOver condition added.

--- Assets/Scripts/Asteroid/destroyByContact.cs
+++ Assets/Scripts/Asteroid/destroyByContact.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class destroyByContact : MonoBehaviour
 {
+    public int scoreValue;
+    private GameController gameController;
+
+    private void Start()
+    {
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "boundary") return;
+        if (other.CompareTag("boundary")) return;
+        if (other.CompareTag("Player"))
+        {
+            gameController.GameOver();
+        }
         Destroy(other.gameObject);
         Destroy(gameObject);
+        gameController.AddScore(scoreValue);
     }
 }
--- Assets/Scripts/Game Controller/GameController.cs
+++ Assets/Scripts/Game Controller/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -11,9 +13,37 @@ public class GameController : MonoBehaviour
     public float startWait;
     public float waveWait;
 
+    private int score;
+    public Text scoreText;
+
+    public Text gameOverText;
+    private bool gameOver;
+    public Button restartButton;
+    private bool restart;
+    public Button backToMenuButton;
+    private bool backToMenu;
+
     void Start()
     {
-       StartCoroutine(SpawnWaves());
+        restart = false;
+        restartButton.gameObject.SetActive(false);
+        gameOver = false;
+        gameOverText.gameObject.SetActive(false);
+        backToMenu = false;
+        backToMenuButton.gameObject.SetActive(false);
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    public void UpdateScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void UpdateMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 
     IEnumerator SpawnWaves()
@@ -28,6 +58,32 @@ public class GameController : MonoBehaviour
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartButton.gameObject.SetActive(true);
+                restart = true;
+                backToMenuButton.gameObject.SetActive(true);
+                backToMenu = true;
+                break;
+            }
         }
+    }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        gameOver = true;
     }
 }
